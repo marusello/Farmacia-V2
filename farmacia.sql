063a9a5213a2f31e31e749b2201dfe388e115af5
gmaciel <conecct@live.com>
2018-11-22 00:08:14 -0200

    Create farmacia.sql
    Banco atualizado com usuario para Login

---
+++ farmacia.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 22-Nov-2018 às 02:05
+-- Versão do servidor: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `farmacia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `caixa`
+--
+
+DROP TABLE IF EXISTS `caixa`;
+CREATE TABLE IF NOT EXISTS `caixa` (
+  `numCaixa` bigint(20) NOT NULL AUTO_INCREMENT,
+  `estado` tinyint(1) NOT NULL,
+  `valorTotal` double NOT NULL,
+  `operador` bigint(20) NOT NULL,
+  PRIMARY KEY (`numCaixa`),
+  KEY `fk_funcionario` (`operador`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `caixa`
+--
+
+INSERT INTO `caixa` (`numCaixa`, `estado`, `valorTotal`, `operador`) VALUES
+(1, 1, 123, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idCliente` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
+  `dataNasc` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estoque`
+--
+
+DROP TABLE IF EXISTS `estoque`;
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `idLote` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nomeProduto` bigint(20) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  PRIMARY KEY (`idLote`),
+  KEY `fk_medicamento` (`nomeProduto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `funId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cargo` varchar(50) NOT NULL,
+  PRIMARY KEY (`funId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`funId`, `nome`, `cargo`) VALUES
+(1, 'João', 'Gerente'),
+(2, 'Maria', 'Atendente');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `medicamento`
+--
+
+DROP TABLE IF EXISTS `medicamento`;
+CREATE TABLE IF NOT EXISTS `medicamento` (
+  `id_medicamento` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `codBarras` varchar(50) NOT NULL,
+  `valorUnit` double NOT NULL,
+  PRIMARY KEY (`id_medicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `idUsuario_UNIQUE` (`idUsuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `login`, `senha`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+CREATE TABLE IF NOT EXISTS `venda` (
+  `codVenda` bigint(20) NOT NULL,
+  `produto` bigint(20) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `subTotal` double NOT NULL,
+  `valorTotal` double NOT NULL,
+  `numCaixa` bigint(20) NOT NULL,
+  `id_cliente` bigint(20) NOT NULL,
+  PRIMARY KEY (`codVenda`),
+  KEY `fk_estoque` (`produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `caixa`
+--
+ALTER TABLE `caixa`
+  ADD CONSTRAINT `fk_funcionario` FOREIGN KEY (`operador`) REFERENCES `funcionario` (`funId`);
+
+--
+-- Limitadores para a tabela `estoque`
+--
+ALTER TABLE `estoque`
+  ADD CONSTRAINT `fk_medicamento` FOREIGN KEY (`nomeProduto`) REFERENCES `medicamento` (`id_medicamento`);
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `fk_estoque` FOREIGN KEY (`produto`) REFERENCES `estoque` (`idLote`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
