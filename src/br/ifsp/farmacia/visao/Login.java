405c75898a8da68ccb0b8eb90e1ce8f25966e66a
gmaciel <conecct@live.com>
2018-11-21 23:58:57 -0200

    Configuração da tela de Login como Main

--- src/br/ifsp/farmacia/visao/Login.java
+++ src/br/ifsp/farmacia/visao/Login.java
@@ -1,10 +1,6 @@
 package br.ifsp.farmacia.visao;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import br.ifsp.farmacia.bd.Acesso;
 
@@ -15,67 +11,44 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
-import javax.swing.border.TitledBorder;
 import java.awt.Color;
-import javax.swing.JLayeredPane;
 import java.awt.Font;
 import java.awt.Toolkit;
 
-public class Login extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField txtLogin;
-	private JPasswordField pwdSenha;
-
-	/**
-	 * Launch the application.
-	 */
+public class Login {
+	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public Login() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\Users\\User\\Desktop\\workspace\\FarmaciaLogin\\src\\imagem\\hospital-2_icon-icons.com_66067.png"));
-		setTitle("Sistema de Software | Farm\u00E1cia");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		
+		// Criando o JFrame
+		JFrame frmLogin = new JFrame("Farmacia");
+		frmLogin.setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/br/ifsp/farmacia/imagem/048-insurance.png")));
+		frmLogin.setTitle("Sistema de Software | Farmacia");
+		frmLogin.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		frmLogin.setSize(444, 377);
+		frmLogin.setLocationRelativeTo(null);
+		frmLogin.getContentPane().setLayout(null);
 		
 		JLabel lblLogin = new JLabel("LOGIN:");
-		lblLogin.setBounds(166, 145, 46, 14);
-		contentPane.add(lblLogin);
+		lblLogin.setBounds(163, 174, 46, 14);
+		frmLogin.getContentPane().add(lblLogin);
 		
-		txtLogin = new JTextField();
-		txtLogin.setBounds(215, 142, 176, 20);
-		contentPane.add(txtLogin);
+		JTextField txtLogin = new JTextField();
+		txtLogin.setBounds(212, 171, 176, 20);
+		frmLogin.getContentPane().add(txtLogin);
 		txtLogin.setColumns(10);
 		
 		JLabel lblSenha = new JLabel("SENHA:");
-		lblSenha.setBounds(166, 176, 46, 14);
-		contentPane.add(lblSenha);
+		lblSenha.setBounds(163, 205, 46, 14);
+		frmLogin.getContentPane().add(lblSenha);
 		
-		pwdSenha = new JPasswordField();
-		pwdSenha.setBounds(215, 173, 176, 20);
-		contentPane.add(pwdSenha);
+		JPasswordField pwdSenha = new JPasswordField();
+		pwdSenha.setBounds(212, 202, 176, 20);
+		frmLogin.getContentPane().add(pwdSenha);
 		
 		/** BOTÃO ENTRAR **/
 		JButton btnEntrar = new JButton("ENTRAR");
 		btnEntrar.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent arg0) {
 				
 				Acesso acesso = new Acesso();
@@ -83,10 +56,8 @@ public class Login extends JFrame {
 				acesso.Logar(txtLogin.getText(), pwdSenha.getText());
 				
 				if(acesso.permissao == true) {
-					MenuPrincipalUI home = new MenuPrincipalUI();
-					home.show();
-					home.setExtendedState(new MenuPrincipalUI().MAXIMIZED_BOTH);
-					dispose();
+					new MenuPrincipalUI();
+					frmLogin.dispose();
 				}
 				
 				if(acesso.permissao == false) {
@@ -98,8 +69,8 @@ public class Login extends JFrame {
 				acesso.permissao = false;
 			}
 		});
-		btnEntrar.setBounds(166, 217, 107, 23);
-		contentPane.add(btnEntrar);
+		btnEntrar.setBounds(163, 260, 107, 23);
+		frmLogin.getContentPane().add(btnEntrar);
 		
 		/** BOTÃO CANCELAR **/
 		JButton btnCancelar = new JButton("CANCELAR");
@@ -110,19 +81,21 @@ public class Login extends JFrame {
 			}
 			
 		});
-		btnCancelar.setBounds(284, 217, 107, 23);
-		contentPane.add(btnCancelar);
+		btnCancelar.setBounds(281, 260, 107, 23);
+		frmLogin.getContentPane().add(btnCancelar);
 		
-		JLabel lblFarmcia = new JLabel("Farm\u00E1cia");
+		JLabel lblFarmcia = new JLabel("Farmacia");
 		lblFarmcia.setForeground(new Color(0, 128, 0));
 		lblFarmcia.setFont(new Font("BlackJack", Font.BOLD, 93));
 		lblFarmcia.setBackground(new Color(255, 69, 0));
-		lblFarmcia.setBounds(24, 16, 389, 115);
-		contentPane.add(lblFarmcia);
+		lblFarmcia.setBounds(21, 45, 397, 115);
+		frmLogin.getContentPane().add(lblFarmcia);
 		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(Login.class.getResource("/imagem/024-pharmacist.png")));
-		label.setBounds(34, 128, 112, 123);
-		contentPane.add(label);
-	}
+		label.setIcon(new ImageIcon(Login.class.getResource("/br/ifsp/farmacia/imagem/024-pharmacist.png")));
+		label.setBounds(31, 171, 107, 122);
+		frmLogin.getContentPane().add(label);
+		
+		frmLogin.setVisible(true);
+		}
 }
